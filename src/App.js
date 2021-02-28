ded13f3f9c563664f16d9900ddf48cbbbba2d834
Munna Hossain <mayinuddin.munna@gmail.com>
2021-02-28 16:32:11 +0600

    population added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import Country from './Components/Country/Country';
+import Cart from './Components/Cart/Cart';
 
 function App() {
     const [countries, setCountries] = useState([]);
+    const [cart, setCart] = useState([]);
+
     useEffect(() => {
         fetch('https://restcountries.eu/rest/v2/all')
             .then(res => res.json())
@@ -13,11 +16,15 @@ function App() {
     }, [])
 
     const addCountry = (country) => {
-        console.log('Country added', country)
+        const newCart = [...cart, country];
+        setCart(newCart);
     }
+
     return (
         <div className="App App-header">
-            <h3>Country Loading: {countries.length}</h3>
+            <h3>Country Loading : {countries.length}</h3>
+            <h4>Country Added {cart.length}: </h4>
+            <Cart cart={cart}></Cart>
             <ul>
                 {
                     countries.map(country => <Country countryInformation={country} addCountry={addCountry}></Country>)
@@ -27,5 +34,4 @@ function App() {
 
     );
 }
-
 export default App;
--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -16,5 +16,4 @@ const Country = (props) => {
         </div>
     );
 };
-
 export default Country;
